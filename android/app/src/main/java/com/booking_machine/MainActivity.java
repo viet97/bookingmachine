2f45074beed23c07d531b58d76de5b806cc76a0b
VietDQ <vietdq@VietDQs-iMac.local>
2022-01-25 01:23:03 -0800

    add app pinning

--- android/app/src/main/java/com/booking_machine/MainActivity.java
+++ android/app/src/main/java/com/booking_machine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.booking_machine;
 
 import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -12,25 +14,31 @@ import androidx.annotation.Nullable;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-
+  private final String[] APP_PACKAGES = {};
+  private DevicePolicyManager dpm;
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     getSupportActionBar().hide();
     super.onCreate(savedInstanceState, persistentState);
+    dpm =
+            (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+    ComponentName adminName = getComponentName();
+    dpm.setLockTaskPackages(adminName, APP_PACKAGES);
   }
 
   @Override
   protected void onPause() {
     super.onPause();
-    ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+   ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 
-    activityManager.moveTaskToFront(getTaskId(), 0);
+   activityManager.moveTaskToFront(getTaskId(), 0);
   }
 
   @Override
   protected void onResume() {
     super.onResume();
+    startLockTask();
     final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
